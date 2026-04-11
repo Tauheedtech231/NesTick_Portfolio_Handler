957bf26e3371ee9fc793ddd057ffd11c921160a8
M.Tauheed <tauheeddeveloper13@gmail.com>
2026-04-11 15:38:46 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import './globals.css'
  // Client wrapper
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "College Portfolio Handler Portal",
@@ -27,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
 
 
